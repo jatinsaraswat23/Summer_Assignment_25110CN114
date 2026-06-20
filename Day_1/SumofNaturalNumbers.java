b1ff422b6b4d69cafb9b40647b933b8b751519cc
jatinsaraswat23 <jatinsaraswat23@gmail.com>
2026-06-20 22:06:27 +0530

    Add files via upload

---
+++ Day_1/SumofNaturalNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumofNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.println("The sum of first " + n + " natural numbers is " + sum);
+
+    }
+}
