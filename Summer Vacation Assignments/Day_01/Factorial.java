0ce12f30b12de9e6b44f8d73ec5c469b4a8e79c8
jatinsaraswat23 <jatinsaraswat23@gmail.com>
2026-06-22 10:36:52 +0530

    Add files via upload

---
+++ Summer Vacation Assignments/Day_01/Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        int product = 1;
+        System.out.println("The factorial of " + n + " is: ");
+        for (int i = 1; i <= n; i++) {
+            product = product * i;
+        }
+        System.out.println(product);
+    }
+}
